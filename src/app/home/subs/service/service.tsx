09a53689f7c606e1685c9a428c96e8ca608e8016
autoclickvn <autoclickvn@gmail.com>
2023-02-18 02:08:23 +0700

    Update UI Home page

--- src/app/home/subs/service/service.tsx
+++ src/app/home/subs/service/service.tsx
@@ -3,7 +3,7 @@ import { Image, Section } from '@/components';
 import { Box, Flex, Icon, Text } from '@chakra-ui/react';
 import Link from 'next/link';
 import { memo } from 'react';
-import { FaAdversal, FaFileContract, FaGlobeAmericas, FaMobileAlt, FaThLarge } from 'react-icons/fa';
+import { FaAdversal, FaFileContract, FaGlobeAmericas, FaMobileAlt, FaPencilRuler, FaThLarge } from 'react-icons/fa';
 import BgService from '../../images/bg-service.png';
 import ImgShadow from '../../images/shadow-service.png';
 
@@ -13,37 +13,43 @@ const HomeService: React.FC = () => {
       title: 'Website',
       icon: FaGlobeAmericas,
       service: 'website',
-      description: 'Thiết kế và xây dựng Website đa nền tảng'
+      description: 'Thiết kế, xây dựng Website đa nền tảng',
+      bgGradient: '#25da25, #1eae1e'
     },
     {
       title: 'Mobile App',
       icon: FaMobileAlt,
       service: 'mobile-app',
-      description: 'Thiết kế và xây dựng Ứng dụng Mobile'
+      description: 'Thiết kế, phát triển Ứng dụng Mobile',
+      bgGradient: '#3fabf3, #0F94EF'
     },
     {
       title: 'Marketing',
       icon: FaFileContract,
       service: 'content-marketing',
-      description: 'Dịch vụ viết nội dung cho Marketing'
+      description: 'Viết nội dung (Content Marketing)',
+      bgGradient: '#9b6ac8, #8D55C0'
     },
     {
       title: 'Advertising',
       icon: FaAdversal,
       service: 'advertising',
-      description: 'Chạy quảng cáo trên mọi nền tảng'
+      description: 'Chạy quảng cáo nhiều nền tảng',
+      bgGradient: '#ffad66, #ff9233'
     },
     {
       title: 'Design',
-      icon: FaThLarge,
+      icon: FaPencilRuler,
       service: 'design',
-      description: 'Thiết kế logo, banner, topic, ảnh...'
+      description: 'Thiết kế logo, banner, topic, ảnh...',
+      bgGradient: '#fe67b0, #FE3B99'
     },
     {
       title: 'Xem thêm',
       icon: FaThLarge,
       service: 'other',
-      description: 'Xem thêm rất nhiều dịch vụ tiện ích khác'
+      description: 'Và rất nhiều dịch vụ tiện ích khác',
+      bgGradient: '#d55d5d, #cb3434'
     }
   ];
 
@@ -51,13 +57,13 @@ const HomeService: React.FC = () => {
     <Flex alignItems="center" flexDirection="column" mt={60} px={60} py={20}>
       <Section title="Proton Service" />
       <Text mt={5} fontSize={20} textAlign="center" color="text.2">
-        Cung cấp các dịch vụ Công Nghệ, Marketing
+        Cung cấp các dịch vụ tiện ích về Công Nghệ, Marketing, Content
       </Text>
 
       <Flex
         w="full"
-        columnGap={20}
         rowGap={10}
+        justify="space-between"
         mt={16}
         flexWrap="wrap"
         bgImage={`url(${BgService.src})`}
@@ -65,20 +71,21 @@ const HomeService: React.FC = () => {
         bgRepeat="no-repeat"
       >
         {SERVICE_DATA.map((item) => {
-          const { icon, title, service, description } = item;
+          const { icon, title, service, description, bgGradient } = item;
+          const iconColor = bgGradient.split(' ')[1];
           return (
-            <Link href={`/service/${service}`} key={title} style={{ display: 'block', width: '28%' }}>
+            <Link href={`/service/${service}`} key={title} style={{ display: 'block', width: '30%' }}>
               <Flex h={80}>
-                <Image src={ImgShadow} h="full" w={20} alt="shadow service" opacity={0.4} />
-                <Flex direction="column" gap={5} h="full" justify="center">
+                <Image src={ImgShadow} h="full" w="70px" alt="shadow service" opacity={0.5} />
+                <Flex direction="column" gap={5} h="full" justify="center" flex={1}>
                   <Box ml={5}>
-                    <Icon as={icon} color="#7f1bb1" fontSize={35} />
+                    <Icon as={icon} color={iconColor} fontSize={35} />
                   </Box>
 
                   <Flex
                     clipPath="polygon(0% 0%, 85% 0%, 100% 50%, 85% 100%, 0% 100%)"
-                    bgColor="red"
-                    w={52}
+                    bgGradient={`linear(to-r, ${bgGradient})`}
+                    w={48}
                     h="70px"
                     align="center"
                     pl={5}
@@ -88,7 +95,7 @@ const HomeService: React.FC = () => {
                     </Text>
                   </Flex>
 
-                  <Text fontSize={18} letterSpacing={0.5} color="text.2" ml={5}>
+                  <Text fontSize={17} letterSpacing={0.5} ml={5} fontWeight={600} h="50px">
                     {description}
                   </Text>
                 </Flex>
